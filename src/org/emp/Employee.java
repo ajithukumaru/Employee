1db593d9d940131183a85913b8cdf92f0f7c080b
ajithukumaru <ajithkumar3531@gmail.com>
2021-11-29 22:57:15 +0530

    employee

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,35 @@
+package org.emp;
+
+public class Employee {
+private void empId() {
+	System.out.println("empId");
+}
+private void empName() {
+	System.out.println("empName");
+}
+private void empPhone() {
+	System.out.println("empPhone");
+}
+public void empMail() {
+	System.out.println("empMail");
+}
+private void empAddress() {
+	System.out.println("empAddress");
+}
+
+
+
+
+
+public static void main(String[]args) {
+	Employee v = new Employee();
+			v.empId();
+			v.empName();
+			v.empPhone();
+			v.empMail();
+			v.empAddress();
+}
+
+
+
+}
